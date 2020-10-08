215d13adfd28fc0d488a162a85017f80eda31fa3
DasVaca <sebastian23082000@gmail.com>
2020-10-08 08:25:26 -0300

    BMP_error: print errno only for system failure

--- BMP_error.h
+++ BMP_error.h
@@ -1,6 +1,9 @@
 void BMP_perror(const char * errmsg)
 {
     fprintf(stderr, "Error: %s", errmsg);
-    fprintf(stderr, "\nstrerror: %s", strerror(errno)); 
+    if (errno)
+    {
+        fprintf(stderr, "\nstrerror: %s", strerror(errno)); 
+    }
     exit(1);
 }
